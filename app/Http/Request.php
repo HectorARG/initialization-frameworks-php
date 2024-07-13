f9ed06d5cbe62a6a4a1caea499e62b4266bcf8fa
HectorARG <hectorr9577@gmail.com>
2024-07-13 17:36:42 -0600

    Se agrega la configuracion para el sent

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -28,4 +28,26 @@ class Request
         // Si el segmento 2 está vacío, asigna el método index por defecto de lo contrario asigna el segmento 2
         $this->method = empty($this->segments[2]) ? 'index' : $this->segments[2];
     }
+
+    public function getController()
+    {
+        // Devuelve el controlador con la primera letra en mayúscula
+        $controller = ucfirst($this->controller);
+        return "App\Http\Controllers\\{$controller}Controller";
+    }
+
+    public function getMethod()
+    {
+        // Devuelve el método
+        return $this->method;
+    }
+
+    public function send()
+    {
+        // Crea una instancia del controlador y llama al método
+        $controller = $this->getController();
+        $method = $this->getMethod();
+        $response = call_user_func([new $controller, $method]);
+        $response->send();
+    }
 }
